f9f3eceb613fdecc910e140a272986ad38aa87e2
liming wu <limwuly@gmail.com>
2019-09-28 16:42:39 -0400

    implement secondary router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './user/auth.service';
 import { Router,Event,NavigationStart,NavigationEnd,NavigationError,NavigationCancel, RouterEvent } from '@angular/router';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -26,7 +27,7 @@ export class AppComponent {
   }
 
   constructor(private authService: AuthService,
-    private router:Router) {
+    private router:Router, private messageService: MessageService) {
       router.events.subscribe((routerEvent:Event)=>{
         this.checkRouterEvent(routerEvent);
       });
@@ -49,5 +50,19 @@ export class AppComponent {
     console.log('Log out');
     //this.router.navigate(['/welcome']); //8th video
     this.router.navigateByUrl('/welcome');
+  }
+
+  displayMessages():void{
+    this.router.navigate([{outlets:{popup:['messages']}}]);
+    this.messageService.isDisplayed=true;
+  }
+
+  hideMessages():void{
+    this.router.navigate([{outlets:{popup:null}}]);
+    this.messageService.isDisplayed=false;
+  }
+
+  get IsMessageDisplayed():boolean{
+    return this.messageService.isDisplayed;
   }
 }
